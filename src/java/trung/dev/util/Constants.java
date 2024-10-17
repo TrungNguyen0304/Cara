4eaee17ebd3d04c56dc295fb332003b4fb5e4cd1
TrungNguyen0304 <TrungNguyen15989@gmail.com>
2024-10-17 14:40:56 +0700

    Remove sensitive information

--- src/java/trung/dev/util/Constants.java
+++ src/java/trung/dev/util/Constants.java
@@ -13,8 +13,8 @@ public class Constants {
     public static final int PER_PAGE = 8;
 
     ////
-    public static final String GOOGLE_CLIENT_ID = "294865909282-djmbb9euq0al6377tiia2t1duhbk9qbt.apps.googleusercontent.com";
-    public static final String GOOGLE_CLIENT_SECRET = "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";
+    public static final String GOOGLE_CLIENT_ID = System.getenv("AUTH_CLIENT_ID");
+    public static final String GOOGLE_CLIENT_SECRET = System.getenv("AUTH_CLIENT_SECRET");
     public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/Cara/LoginServlet";
     public static final String GOOGLE_GRANT_TYPE = "authorization_code";
     public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
